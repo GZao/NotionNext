1efc74fec97d714052a6b5c77d88109d803f698d
GZao <72484466+GZao@users.noreply.github.com>
2025-06-15 12:01:02 +0200

    Update Announcement.js

--- themes/hexo/components/Announcement.js
+++ themes/hexo/components/Announcement.js
@@ -3,19 +3,27 @@ import dynamic from 'next/dynamic'
 
 const NotionPage = dynamic(() => import('@/components/NotionPage'))
 
-const Announcement = ({ post, className }) => {
+const Announcement = ({ notice }) => {
   const { locale } = useGlobal()
-  if (post?.blockMap) {
-    return <div className={className}>
-        <section id='announcement-wrapper' className="dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray">
-            <div><i className='mr-2 fas fa-bullhorn' />{locale.COMMON.ANNOUNCEMENT}</div>
-            {post && (<div id="announcement-content">
-            <NotionPage post={post} className='text-center' />
-        </div>)}
-        </section>
-    </div>
-  } else {
+  if (!notice || Object.keys(notice).length === 0) {
     return <></>
   }
+  return <div className="px-3 w-full">
+        <div
+            data-aos="zoom-in"
+            data-aos-duration="500"
+            data-aos-once="true"
+            data-aos-anchor-placement="top-bottom"
+            className="mb-4 p-2 overflow-auto shadow-md border dark:border-black rounded-xl bg-white dark:bg-hexo-black-gray">
+            <div className="text-sm flex flex-nowrap justify-between">
+                <div className="font-light text-gray-600  dark:text-gray-200">
+                    <i className="mx-2 fas fa-bullhorn" />{locale.COMMON.ANNOUNCEMENT}
+                </div>
+            </div>
+            {notice && (<div id="announcement-content">
+                <NotionPage post={notice} className='text-center ' />
+            </div>)}
+        </div>
+    </div>
 }
 export default Announcement
