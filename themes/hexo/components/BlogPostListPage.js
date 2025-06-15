4cac67c5ccf8643bc0511f50f98c07e67b5a7814
GZao <72484466+GZao@users.noreply.github.com>
2025-06-15 12:17:50 +0200

    Update BlogPostListPage.js

--- themes/hexo/components/BlogPostListPage.js
+++ themes/hexo/components/BlogPostListPage.js
@@ -1,8 +1,9 @@
-import { siteConfig } from '@/lib/config'
-import { useGlobal } from '@/lib/global'
-import BlogPostCard from './BlogPostCard'
-import BlogPostListEmpty from './BlogPostListEmpty'
-import PaginationNumber from './PaginationNumber'
+import { siteConfig } from '@/lib/config';
+import { useGlobal } from '@/lib/global';
+import BlogPostCard from './BlogPostCard';
+import BlogPostListEmpty from './BlogPostListEmpty';
+import PaginationNumber from './PaginationNumber';
+import Announcement from './Announcement'; // 新增导入
 
 /**
  * 文章列表分页表格
@@ -13,15 +14,18 @@ import PaginationNumber from './PaginationNumber'
  * @constructor
  */
 const BlogPostListPage = ({ page = 1, posts = [], postCount, siteInfo }) => {
-  const { NOTION_CONFIG } = useGlobal()
-  const POSTS_PER_PAGE = siteConfig('POSTS_PER_PAGE', null, NOTION_CONFIG)
-  const totalPage = Math.ceil(postCount / POSTS_PER_PAGE)
-  const showPagination = postCount >= POSTS_PER_PAGE
+  const { NOTION_CONFIG, locale, notice } = useGlobal(); // 假设 notice 可用
+  const POSTS_PER_PAGE = siteConfig('POSTS_PER_PAGE', null, NOTION_CONFIG);
+  const totalPage = Math.ceil(postCount / POSTS_PER_PAGE);
+  const showPagination = postCount >= POSTS_PER_PAGE;
+
   if (!posts || posts.length === 0 || page > totalPage) {
-    return <BlogPostListEmpty />
+    return <BlogPostListEmpty />;
   } else {
     return (
       <div id='container' className='w-full'>
+        {/* 添加 Announcement 在文章列表上方 */}
+        <Announcement post={notice} className="mb-6" /> {/* 使用 notice 作为 post */}
         {/* 文章列表 */}
         <div className='space-y-6 px-2'>
           {posts?.map(post => (
@@ -37,8 +41,8 @@ const BlogPostListPage = ({ page = 1, posts = [], postCount, siteInfo }) => {
           <PaginationNumber page={page} totalPage={totalPage} />
         )}
       </div>
-    )
+    );
   }
-}
+};
 
-export default BlogPostListPage
+export default BlogPostListPage;
